6c2f28291b28957a6d1b193a8e75221324591c92
Damagucci-Juice <jsr9874@gmail.com>
2025-07-06 23:55:32 +0900

    return state parsing

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -79,7 +79,7 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	return true
 }
 
-func TestReturnStatements(t *testing T) {
+func TestReturnStatements(t *testing.T) {
 	input := `
 	return 5;
 	return 10;
@@ -90,13 +90,21 @@ func TestReturnStatements(t *testing T) {
 	p := New(l)
 
 	program := p.ParseProgram()
-	checkParserErrors()
+	checkParserErrors(t, p)
 
 	if len(program.Statements) != 3 {
 		t.Fatalf("Program. Statements does not contain 3 statements. got=%d", len(program.Statements))
 	}
 
 	for _, stmt := range program.Statements {
-		
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got =%T", stmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got=%q", returnStmt.TokenLiteral())
+		}
 	}
 }
